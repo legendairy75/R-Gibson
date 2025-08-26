21935f28674d5e686677f4da92d653d54a653889
legendairy75 <roncrash1@gmail.com>
2025-08-26 14:32:10 -0500

    fix navbar

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -9,7 +9,7 @@ export default defineConfig({
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      // { text: 'Blog', link: '/Blog' }
+      { text: 'Blog', link: '/Blog' }
     ],
     logo: '/favicon.svg',
 

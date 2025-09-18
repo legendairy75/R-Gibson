feb05002b02c688117ac1a099f5bfec7fed1b1c7
Cameron Gibson <36999898+legendairy75@users.noreply.github.com>
2025-09-18 18:41:12 -0500

    Delete configs directory

--- configs/nvim/.config/nvim/lua/plugins/lsp.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "olrtg/nvim-emmet",
-    config = function()
-      vim.keymap.set({ "n", "v" }, "<leader>xe", require("nvim-emmet").wrap_with_abbreviation)
-    end,
-  },
-  require("luasnip.loaders.from_vscode").lazy_load({ paths = "~/.config/nvim/my_snippets" }),
-}

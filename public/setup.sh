f45d303bd3eb6abcc8a4f5029d08bdf6835432cc
legendairy75 <roncrash1@gmail.com>
2025-09-23 11:41:13 -0500

    added echos

--- public/setup.sh
+++ public/setup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+echo "Caraline Setup Script"
+echo "\nUpdating!"
+
 sudo dnf update
 
+echo "\nInstalling packages!"
 pkgs=(
   kitty
   zsh
@@ -19,11 +23,16 @@ pkgs=(
 )
 for pkg in ${pkgs[@]}; do
   sudo dnf -y install ${pkg}
+  echo "${pkg} installed"
 done
 
+echo "This is where the fun begins"
+
+echo "\nInstalling ohmyzsh"
 # oh my zsh
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 
+echo "\nInstalling oh my posh and nerd font"
 # oh my posh
 curl -s https://ohmyposh.dev/install.sh | bash -s
 
@@ -34,11 +43,13 @@ wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/do
   rm 0xProto.zip &&
   fc-cache -fv
 
+echo "\nInstalling lazyvim"
 # lazyvim
 git clone https://github.com/LazyVim/starter ~/.config/nvim
 
 rm -rf ~/.config/nvim/.git
 
+echo "\nInstalling dotfiles"
 # dotfiles
 
 git clone https://github.com/legendairy75/.dotfiles.git ~/.dotfiles
@@ -62,6 +73,7 @@ stow yazi
 # dnf check-update
 # sudo dnf install code # or code-insiders
 
+echo "\nInstalling homebrew"
 # Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
@@ -69,9 +81,11 @@ echo >>/home/$USER/.bashrc
 echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >>/home/$USER/.bashrc
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
+echo "\nInstalling vfox"
 # vfox
 brew install vfox
 
+echo "\nInstalling node & pnpm"
 # node
 vfox add nodejs
 
@@ -80,13 +94,18 @@ vfox install nodejs@latest
 # pnpm
 sudo npm install -g pnpm@latest-10
 
-pnpm add -g figlet
 pnpm add -g nodemon
 
+echo "\nInstalling yazi"
 # yazi
 sudo dnf copr enable -y lihaohong/yazi
 sudo dnf install -y yazi
 
-chsh -s /bin/zsh
+echo "\nchoose default applications"
+kcmshell6 componentchooser
 
+echo "making zsh default shell"
+chsh -s $(which zsh)
+
+echo "\nAll done! Rebooting in 5 seconds..."
 sudo reboot

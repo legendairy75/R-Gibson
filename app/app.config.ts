8b4a9df32e4a7b461999a7c6fdcb7c5d8acc82ea
legendairy <legendairy975@gmail.com>
2026-09-24 08:45:14 -0500

    added custome theme

---
+++ app/app.config.ts
@@ -0,0 +1,18 @@
+export default defineAppConfig({
+  ui: {
+    colors: {
+      primary: 'mauve',
+      secondary: 'pink',
+      success: 'green',
+      info: 'blue',
+      warning: 'yellow',
+      error: 'red',
+      neutral: 'ctp'
+    },
+    button: {
+      defaultVariants: {
+        variant: 'subtle'
+      }
+    }
+  }
+})
